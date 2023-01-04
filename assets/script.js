5058c154a25c9240f8c97360b421db12ccdd7f4f
byunn90 <kayhanturk90@Live.com>
2023-01-04 22:29:51 +1100

    added api's and function

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,27 @@
 console.log("hi");
+// const mycords = navigator.geolocation;
+// console.log(mycords);
+
+function getWeatherForcast(lat, lon) {
+  const myApiKey = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=54f233828acf58994eefa05b9027dd89`;
+  console.log(lat, lon);
+  fetch(myApiKey)
+    .then((response) => response.json())
+    .then((response) => console.log(response));
+}
+
+function getGeolocation() {
+  const cityName = "melbourne";
+  const getMyLatLong = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=54f233828acf58994eefa05b9027dd89`;
+
+  fetch(getMyLatLong)
+    .then((response) => response.json())
+    .then((response) => {
+      const lat = response[0].lat;
+      const lon = response[0].lon;
+      getWeatherForcast(lat, lon);
+    });
+}
+
+// getWeatherForcast();
+getGeolocation();
