ce2bed104da2b519db37e3c337ee930bc87edfe0
byunn90 <kayhanturk90@Live.com>
2023-01-24 23:32:15 +1100

    finished button thats clears the innerHTML div for the local storage

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,10 @@
 const buttonClick = document.getElementById("btn-control");
-const textBoxAreaHandler = document.getElementById("text-box");
+// const textBoxAreaHandler = document.getElementById("text-box");
 const container = document.querySelector(".container-block");
 const container2 = document.querySelector(".container");
 const unOrderList = document.querySelector("first-data-box");
+const searchHistory = document.querySelector(".history-List");
+let inputTextContent = document.getElementById("text-box");
 
 function Objectives() {
   /*
@@ -13,12 +15,9 @@ function Objectives() {
   we have to make a local storage everytime we search for a place
   we have to make a box area for the main city/country and that day 
   weather
-
-  
   */
 }
 const formSubmitHandler = function (e) {
-  let inputTextContent = document.getElementById("text-box");
   e.preventDefault();
   const mainContainer = document.querySelector(".container");
   container.innerHTML = "";
@@ -30,12 +29,44 @@ const formSubmitHandler = function (e) {
     "url('https://source.unsplash.com/2560x1440/?" + language + "')";
   getGeolocation(language);
   mainContainer.innerHTML = "";
-  console.log(language);
   container2.classList.remove("hidden");
+  const newArray = JSON.parse(localStorage.getItem("Searched History")) || [];
+  newArray.push(language);
+  const storage = inputTextContent.value;
+  const myKeyValue = "Searched History";
+  localStorage.setItem(myKeyValue, JSON.stringify(newArray));
+  const gettingName = localStorage.getItem(myKeyValue, storage);
+  console.log(storage);
+  console.log(gettingName);
+  let storageArray = JSON.parse(localStorage.getItem("Searched History")) || [];
+  searchHistory.innerHTML = "";
+  // Refactor this code Please
+  // Make functions
+  for (let index = 0; index < storageArray.length; index++) {
+    console.log(storageArray[index]);
+    let li = document.createElement("button");
+    li.classList.add("historyButton");
+    li.textContent = storageArray[index];
+    searchHistory.appendChild(li);
+  }
 };
+// let historyButtons = document.querySelectorAll(".historyButton");
+// for (let index = 0; index < historyButtons.length; index++) {
+//   console.log("hi");
+//   historyButtons[index].addEventListener("click", helloworld);
+//   // getGeolocation(historyButtons[index].textContent)
+// }
+// function helloworld() {
+//   console.log("hi");
+//   getGeolocation(this.innerHTML);
+// }
+
+const localStorageF = function () {};
+
 const getWeatherForcast = function (lat, lon) {
   const myApiKey = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=days&appid=54f233828acf58994eefa05b9027dd89`;
   console.log(lat, lon);
+  language = inputTextContent.value;
   fetch(myApiKey)
     .then((response) => response.json())
     .then((response) => {
@@ -51,6 +82,7 @@ const getWeatherForcast = function (lat, lon) {
           class="country-img"
           src="http://openweathermap.org/img/wn/${day.weather[0].icon}.png"
           ></img>
+          <h1>${language.charAt(0).toUpperCase() + language.slice(1)}</h1>
           <h1 class="dates">${date.toString().substring(0, 16)}</h1>
           <h2 class="city-country-temps">
           temp: ${(day.feels_like.day - 273.15).toFixed(1)}°C
@@ -71,10 +103,11 @@ const getWeatherForcast = function (lat, lon) {
         container.insertAdjacentHTML(
           "beforeend",
           `
-            <div class="country-data-2">
+            <div class="country-data-3">
           <img class="country-img" src="http://openweathermap.org/img/wn/${
             day.weather[0].icon
           }.png"></img>
+          <h1>${language.charAt(0).toUpperCase() + language.slice(1)}</h1>
           <h1 class="dates">${date.toString().substring(0, 16)}</h1>
           <h2 class="city-country-temps">temp: ${(
             day.feels_like.day - 273.15
@@ -91,7 +124,7 @@ const getWeatherForcast = function (lat, lon) {
     });
 };
 
-const getGeolocation = function (user) {
+function getGeolocation(user) {
   const getMyLatLong = `http://api.openweathermap.org/geo/1.0/direct?q=${user}&limit=1&appid=54f233828acf58994eefa05b9027dd89`;
 
   fetch(getMyLatLong)
@@ -103,10 +136,10 @@ const getGeolocation = function (user) {
         getWeatherForcast(lat, lon);
       }
     });
-};
+}
 
 buttonClick.addEventListener("click", formSubmitHandler);
-
+localStorageF();
 const checkingTwo = function () {
   const lat = -37.8142176;
   const lon = 144.9631608;
